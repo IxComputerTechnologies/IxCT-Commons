2dab1c821193d0be5d901040242d765d5f3afc69
MrWooly357 <mrwooly357@gmail.com>
2026-05-10 01:52:21 +0300

    Added copyright.

--- src/main/kotlin/util/Profiler.kt
+++ src/main/kotlin/util/Profiler.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright © 2026 IxComputerTechnologies (a.k.a. IxCT) and Belov Ivan Alekseevich (a.k.a. MrWooly357). Licensed under the MIT licence.
+ */
+
 package net.ixct.ixct_commons.util
 
 class Profiler : AutoCloseable {
